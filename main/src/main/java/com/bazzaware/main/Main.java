b44799ba7a50d1476245fb4ff9084b3abdeb269d
Bazza-MacBook <bazzaware@gmail.com>
2023-10-19 16:17:30 +0100

    Working in Netbeans

--- main/src/main/java/com/bazzaware/main/Main.java
+++ main/src/main/java/com/bazzaware/main/Main.java
@@ -1,23 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.bazzaware.main;
 
 import com.example.Car;
 import com.example.Cat;
 
-/**
- *
- * @author bazza
- */
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Car car01 = new Car("Ford", "Capri");
-        Cat cat01 = new Cat("Tom","Gray");
-        
+        Cat cat01 = new Cat("Tom", "Gray");
+
         System.out.println(car01);
         System.out.println(cat01);
     }
