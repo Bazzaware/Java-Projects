73799d988d6cf0be0c56b454439297e5d246840a
Bazza-MacBook <bazzaware@gmail.com>
2023-10-19 16:23:14 +0100

    Typo

--- main/src/main/java/com/bazzaware/main/Main.java
+++ main/src/main/java/com/bazzaware/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Car car01 = new Car("Ford", "Capri");
-        Cat cat01 = new Cat("Tom", "Gray");
+        Cat cat01 = new Cat("Tom", "Grey");
 
         System.out.println(car01);
         System.out.println(cat01);
