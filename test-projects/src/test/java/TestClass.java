07c2312d12660f2ada001573a3c8f7ba3bd6dbac
Bazza-MacBook <bazzaware@gmail.com>
2023-10-19 15:21:34 +0100

    passing tests

--- test-projects/src/test/java/TestClass.java
+++ test-projects/src/test/java/TestClass.java
@@ -1,20 +1,16 @@
-package com.example;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+
+public class TestClass {
+    
+    public TestClass() {
+     
+    }
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue(true);
+        assertTrue( true );
     }
 }
