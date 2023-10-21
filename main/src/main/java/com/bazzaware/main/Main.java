d2d02b1239e00f0a07df0f613798f573e7a32a72
Bazza-MacBook <bazzaware@gmail.com>
2023-10-21 09:13:03 +0100

    Working in both netbeans and Visual code

--- main/src/main/java/com/bazzaware/main/Main.java
+++ main/src/main/java/com/bazzaware/main/Main.java
@@ -1,16 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
 package com.bazzaware.main;
 
-import com.example.Car;
-import com.example.Cat;
+import com.bazzaware.member.a.Car;
+import com.bazzaware.member.b.Cat;
 
+/**
+ *
+ * @author bazza
+ */
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Car car01 = new Car("Ford", "Capri");
-        Cat cat01 = new Cat("Tom", "Grey");
+        Cat cat01 = new Cat("Thomas", "Grey");
 
         System.out.println(car01);
         System.out.println(cat01);
+
+        System.out.println(car01.makeNoise());
+        System.out.println(cat01.makeNoise());
     }
 }
